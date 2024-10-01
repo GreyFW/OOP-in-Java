2d77a0a569ef14fbae1a67fb84b10e3a58829e4e
GreyF <greysforgettea@gmail.com>
2024-10-01 23:22:13 +0700

    2nd commit,Main modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите номер задания: ");
+        int taskNumber;
+
+        if (scanner.hasNextInt()) {
+            taskNumber = scanner.nextInt();
+        } else {
+            System.out.println("Некорректный ввод.");
+            return;
+        }
+
+        switch (taskNumber) {
+            case 1: {
+
+                break;
+            }
+
+            case 2: {
+
+                break;
+            }
+
+            case 3: {
+
+                break;
+            }
+        }
+    } // end of main() func;
+
+} // end of Main Class;
